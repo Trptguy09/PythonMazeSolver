0caa0fe376071f2d7473f3c3ab45698dab5d2f3e
Trptguy09 <nervouspuddin@gmail.com>
2026-03-26 12:56:18 -0400

    final commit for boot.dev mazesolving project. Create a random maze with given parameters and then solve said maze. May optimize more and add additional features in the future

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     # set window size
     win = Window(1000, 1000)
     maze = Maze(100, 100, 25, 25, 32, 32, win)  # 800 ratio leaves border
-
+    maze.solve()
     win.wait_for_close()
 
 
--- maze.py
+++ maze.py
@@ -116,5 +116,71 @@ class Maze:
             for j in range(self.__num_rows):
                 self.__cells[i][j].visited = False
 
+    def _solve_r(self, i, j):
+
+        self.__animate()
+        current_cell = self.__cells[i][j]
+        current_cell.visited = True
+
+        if (i, j) == (self.__num_cols - 1, self.__num_rows - 1):
+            return True
+
+        # left
+
+        if (
+            i > 0
+            and not current_cell.has_left_wall
+            and self.__cells[i - 1][j].visited is False
+        ):
+            target_cell = self.__cells[i - 1][j]
+            current_cell.draw_move(target_cell)
+            if self._solve_r(i - 1, j):
+                return True
+            else:
+                current_cell.draw_move(target_cell, undo=True)
+
+        # right
+
+        if (
+            i < self.__num_cols - 1
+            and not current_cell.has_right_wall
+            and self.__cells[i + 1][j].visited is False
+        ):
+            target_cell = self.__cells[i + 1][j]
+            current_cell.draw_move(target_cell)
+            if self._solve_r(i + 1, j):
+                return True
+            else:
+                current_cell.draw_move(target_cell, undo=True)
+
+        # up
+
+        if (
+            j > 0
+            and not current_cell.has_top_wall
+            and self.__cells[i][j - 1].visited is False
+        ):
+            target_cell = self.__cells[i][j - 1]
+            current_cell.draw_move(target_cell)
+            if self._solve_r(i, j - 1):
+                return True
+            else:
+                current_cell.draw_move(target_cell, undo=True)
+
+        # down
+
+        if (
+            j < self.__num_rows - 1
+            and not current_cell.has_bottom_wall
+            and self.__cells[i][j + 1].visited is False
+        ):
+            target_cell = self.__cells[i][j + 1]
+            current_cell.draw_move(target_cell)
+            if self._solve_r(i, j + 1):
+                return True
+            else:
+                current_cell.draw_move(target_cell, undo=True)
+        return False
+
     def solve(self):
         return self._solve_r(0, 0)
